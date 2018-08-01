861a181aee2a7c5bb4576cd6546dee79998d5364
Tunahan İLHAN <tunahann40@gmail.com>
2018-08-01 12:58:33 +0300

    Add files via upload

---
+++ CrossSiteScriptingRequestWrapper.java
@@ -0,0 +1,52 @@
+package com.utils.filters;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+public final class CrossSiteScriptingRequestWrapper extends HttpServletRequestWrapper {
+
+    public CrossSiteScriptingRequestWrapper(HttpServletRequest servletRequest) {
+        super(servletRequest);
+    }
+
+    public String[] getParameterValues(String parameter) {
+        String[] values = super.getParameterValues(parameter);
+        if (values == null) {
+            return null;
+        }
+        int count = values.length;
+        String[] encodedValues = new String[count];
+        for (int i = 0; i < count; i++) {
+            encodedValues[i] = cleanXSS(values[i]);
+        }
+        return encodedValues;
+    }
+
+    public String getParameter(String parameter) {
+        String value = super.getParameter(parameter);
+        if (value == null || value.equalsIgnoreCase("undefined")) {
+            return null;
+        }
+        return cleanXSS(value);
+    }
+
+    public String getHeader(String name) {
+        String value = super.getHeader(name);
+        if (value == null)
+            return null;
+        return cleanXSS(value);
+    }
+
+    private String cleanXSS(String value) {
+        value = value.replaceAll("<", "& lt;").replaceAll(">", "& gt;");
+        value = value.replaceAll("'", "& #39;");
+        value = value.replaceAll("eval\\((.*)\\)", "");
+        value = value.replaceAll("[\\\"\\\'][\\s]*javascript:(.*)[\\\"\\\']", "\"\"");
+        value = value.replaceAll("script", "");
+		if( ! value.equalsIgnoreCase(cleanXSS(value)))
+		{
+		    value = cleanXSS(value);
+		}
+        return value;
+    }
+}
